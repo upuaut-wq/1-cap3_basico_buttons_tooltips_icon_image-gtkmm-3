2b3625c3c7b8fdf62c4a23b3501a6c4a764dd432
Leonardo de Jesus Queiroz <upuaut@tutanota.com>
2021-11-06 15:08:55 -0300

    Completa

--- botao.cpp
+++ botao.cpp
@@ -1,7 +1,7 @@
 #include "botao.h"
 #include <iostream>
 
-Botao::Botao(){
+Botao::Botao():m_tooltip_p(Gtk::WINDOW_POPUP){ //Necessario definir o tipo de janela no constructor.
     //Configuração da Window
     this->set_border_width(10); //Borda entre o limite da window e o objeto.
 
@@ -20,7 +20,7 @@ Botao::Botao(){
     this->m_box1.pack_start(this->m_btn3);
     this->m_box1.pack_start(this->m_btn4);
 
-    //Propriedades do Botão 1 ******************************************************************************************************
+    //BOTÃO1 ******************************************************************************************************
     this->m_btn1.set_focus_on_click(false); //Não permite foco quando utilizado CTRL+Click. 
     this->m_btn1.set_tooltip_markup("<span foreground=\"blue\" size=\"x-large\">Este texto é azul</span> isso é <i>LEGAL </i>!");
     //Veja mais https://docs.gtk.org/Pango/pango_markup.html
@@ -32,10 +32,33 @@ Botao::Botao(){
     this->m_btn1.set_always_show_image(true);
 
 
-    //Propriedades do Botão 2 ******************************************************************************************************
+    //BOTÃO2 ******************************************************************************************************
     this->m_btn2.set_can_focus(false); //Não permite o foco quando precionado o TAB;
+    this->m_btn2.set_label("Botão2");
+    this->m_btn2.set_tooltip_window(this->m_tooltip_p); //Adiciona um tooltip do tipo window ao botao.
 
-    //Propriedades do Botão 3
+    //Button's custom tooltip window:
+    this->m_tooltip_p.set_default_size(0, 0);  //Tamanho do tooltip.0 = se ajusta ao conteudo.
+    this->m_scale.set_range(0,100);               //Intervalo do Scale.
+    
+    //Para TEXTO
+    //Gtk::Label* label =
+    //Gtk::make_managed<Gtk::Label>("A label in a custom tooltip window");
+    //label->show();
+
+    //Para WIDGET(Aparenta funcionar)
+    //this->m_tooltip_p.add(this->m_scale);   //Adiciona a window o scale.
+    //this->m_scale.show();  //Deve estar presente, aparentemente o show_all_children da view principal não alcança o scale.
+
+    //Para IMAGE
+    this->m_tooltip_p.add(this->m_img_t);   //Adiciona a window o scale.
+    this->m_img_t.set_from_icon_name("gtk-media-forward",Gtk::ICON_SIZE_DIALOG);
+    this->m_img_t.show();  //Deve estar presente, aparentemente o show_all_children da view principal não alcança o scale.
+
+    //O tooltip aparece muito rapido,não é possivel alterar o valor do scale.
+    this->m_btn2.signal_query_tooltip().connect(sigc::mem_fun(*this, &Botao::on_button_query_tooltip)); //Sinal gerado pelo tooltip.
+
+    //BOTÃO3 ******************************************************************************************************
     this->m_btn3.set_vexpand(true); //Ativa a expanção do objeto para ocupar espaço sobrando.
                //set_hexpand(true)  <- versão horizontal
 
@@ -48,10 +71,21 @@ Botao::Botao(){
 
     this->m_btn3.set_size_request(100,100); //So funciona se estiver fora do ALIGN_FILL.
     this->m_btn3.set_margin_start(10); //Adiciona uma margem no objeto.Possui para todos os lados.
+    this->m_btn3.set_margin_end(10); //Adiciona uma margem no objeto.Possui para todos os lados.
     
     //Adiciona somente icone ao botão.
-    //PAREI AQUI
-                                        
+    this->ref_icon_bt3 = Gdk::Pixbuf::create_from_file("icon.png",100,100,false); //Cria uma referencia.
+    this->m_img.set(this->ref_icon_bt3);                                          //Adiciona a referencia em um widget do tipo image.
+    this->m_btn3.set_image(this->m_img);                                          //Adiciona a imagem no botão.
+    //this->m_btn3.set_label("Btn3");            < Adiciona um Label
+    //this->m_btn3.set_always_show_image(true);  < Mostra tbm o label
+
+    this->m_btn3.set_tooltip_text("  -Texto normal sem tags de formatação.\n  -Imagem personalizada no icone.\n  -Margem esq,dir de 10px.\n  -Alinhamento no inicio com vspand.\n -Nome pode ser colocado tbm.");
+
+    //BOTÃO3 ******************************************************************************************************
+    this->m_btn4.set_label("Altera");
+    this->m_btn4.signal_clicked().connect(sigc::mem_fun(*this,&Botao::on_btn4_alt_img)); //Sinal, quando acionado altera imagem.                      
+    this->m_btn4.set_tooltip_text("Altera a imagem do tooltipWindow e do botao1");
 
 
     this->show_all_children();
@@ -61,3 +95,27 @@ Botao::Botao(){
 Botao::~Botao(){
 
 }
+
+
+
+bool Botao::on_button_query_tooltip(int, int, bool, const Glib::RefPtr<Gtk::Tooltip>&)
+{
+  //We already have a custom window ready, just return true to show it:
+  
+  return true;
+}
+
+
+    void  Botao::on_btn4_alt_img(){
+        //Na documentação é recomendado utilizar o Ustring ao inves de string.
+        // Relacionado a caracteres de algumas linguagens(Tradução)->rever...
+        Glib::ustring n_icon[5] = {"gtk-media-forward","gtk-dialog-warning","gtk-page-setup","gtk-refresh","gtk-add"};
+        std::srand(std::time(0));
+        int x = rand() % 5;
+        std::cout << x << std::endl;
+        std::cout << n_icon[x] << std::endl;
+
+        //Realiza a alteração
+        this->m_img_t.set_from_icon_name(n_icon[x],Gtk::ICON_SIZE_DIALOG);
+        this->m_btn1.set_image_from_icon_name(n_icon[x]);
+    }
--- botao.h
+++ botao.h
@@ -5,6 +5,8 @@
 #include <gtkmm/button.h>
 #include <gtkmm/image.h>
 #include <gdkmm/pixbuf.h>
+#include <gtkmm/scale.h>
+#include <gtkmm/label.h>
 
 class Botao : public Gtk::Window
 {
@@ -14,14 +16,27 @@ public:
 
 protected:
     //Sinais
-
+     bool on_button_query_tooltip(int x, int y, bool keyboard_tooltip, const Glib::RefPtr<Gtk::Tooltip>& tooltip);
+     void on_btn4_alt_img();
     //Widgets 
     Gtk::Box m_box1; 
     Gtk::Button m_btn1;
     Gtk::Button m_btn2;
     Gtk::Button m_btn3;
     Gtk::Button m_btn4;
+
+    //Tool Tip Personalizado
+    Gtk::Window m_tooltip_p;
+    Gtk::Scale m_scale;
+    Gtk::Label m_label;
+    Gtk::Image m_img_t;
+
+
+    //Referencia e imagem para botão 3
+    Glib::RefPtr<Gdk::Pixbuf> ref_icon_bt3;
     Gtk::Image m_img;
+
+
 };
 
 #endif //GTKMM_BOTAO_H
